7faf3ee139e85c8a6b3673780f7714bbb93e3377
sal68-hockey <526smontecalvo@frhsd.com>
2025-12-05 10:24:28 -0500

    json with chat add 3 movies

--- site.js
+++ site.js
@@ -21,24 +21,50 @@
 */
 
 
+
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+            return {
+                  // This holds your movies.json data.
+                  movies: [],
+                  title: "",
+                  owner: "Sal Montecalvo",
+                  github: "fill with mine or your github username",
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+
+            }
+      },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            makeTextDate(dateArray) {
+                  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                  let year = dateArray[0];
+                  let month = months[dateArray[1] - 1];
+                  let day = dateArray[2];
+                  return month + " " + day + ", " + year; },
+            like(index) {
+                  this.movies[index].likes++
+            },
+            dislike(index) {
+                  this.movies[index].dislikes++
+
+            },
+            posterClick(index) {
+            let movie = this.movies[index];
+                  movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
+            },
+            timeText(minutes) {
+             let hrs = Math.floor(minutes / 60);
+                 let mins = minutes % 60;
+                  return hrs + "h " + mins + "min";
+            }
+      
       }
 })
 
